e8a35d54989f138ccb713b4b6b84b4c9e5fb1592
minhducquach <minhduc16102@gmail.com>
2026-03-30 16:43:45 +0200

    test rt

--- playground/capture.py
+++ playground/capture.py
@@ -1,6 +1,7 @@
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import onnxruntime as ort
 
 def depth_to_color_opencv(depth_map, vmin=None, vmax=None, colormap=cv2.COLORMAP_TURBO):
     """
@@ -55,6 +56,8 @@ print("Depth Scale is: " , depth_scale)
 align_to = rs.stream.color
 align = rs.align(align_to)
 
+session = ort.InferenceSession("../convert-onnx/model.onnx", providers=['CUDAExecutionProvider'])
+
 while True:
     frame = pipe.wait_for_frames()
 
@@ -63,16 +66,45 @@ while True:
     depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
 
-    depth_image = np.asanyarray(depth_frame.get_data())
+    depth_image = np.asanyarray(depth_frame.get_data()).astype(np.float32)
     color_image = np.asanyarray(color_frame.get_data())
+
+    img_np = (color_image / 255.0).astype(np.float32)
+    img_np = np.transpose(img_np, (2, 0, 1))
+    img_np = np.expand_dims(img_np, axis=0)
+
+    depth_np = np.expand_dims(depth_image * depth_scale, axis=0)
+    depth_np = np.expand_dims(depth_np, axis=0)
+
+    num_tokens_np = np.array(1200, dtype=np.int64)
+
+    inputs = {
+        "image": img_np,
+        "num_tokens": num_tokens_np,
+        "depth": depth_np
+    }
+
+    outputs = session.run(None, inputs)
+    depth_out = outputs[0].squeeze()
+    mask_out = outputs[1].squeeze()
+
+    binary_mask = mask_out > 0.5
+
+    depth_out = np.where(binary_mask, depth_out, np.inf)
+    depth_pred = depth_to_color_opencv(depth_out)
 
     depth_m = depth_to_color_opencv(depth_image * depth_scale)
     # gray_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 
+    # print(depth_image)
+    # print(color_image)
+    # break
+
     cv2.imshow('rgb', color_image)
     cv2.imshow('depth', depth_m)
+    cv2.imshow('pred', depth_pred)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(33) == ord('q'):
         np.save("depth.npy", depth_image * depth_scale)
         cv2.imwrite("depth.png", depth_m)
         cv2.imwrite("color.png", color_image)
